c50a94113f8f6d2ff2bfe8ef99d61053bf466d9a
Timothy jan <jan.timothy@gmail.com>
2024-11-01 17:21:08 -0700

    Department and Role create forms complete

---
+++ src/app/components/department-create/department-create.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
+import { DepartmentService } from '../../services/department.service';
+
+@Component({
+  selector: 'app-department-create',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './department-create.component.html',
+  styleUrls: ['./department-create.component.css']
+})
+export class DepartmentCreateComponent {
+  departmentForm: FormGroup = new FormGroup({
+    departmentName: new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(50)])
+  });
+
+  constructor(private _departmentService: DepartmentService) {}
+
+  onSubmit(): void {
+    if (this.departmentForm.valid) {
+      console.log('Form Submitted:', this.departmentForm.value);
+      this._departmentService.addDepartment(this.departmentForm.value);
+      this.departmentForm.reset();
+    }
+  }
+}
---
+++ src/app/components/role-create/role-create.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
+import { RoleService } from '../../services/role.service';
+import { DepartmentService } from '../../services/department.service';
+import { Department } from '../../models/department.model';
+
+@Component({
+  selector: 'app-role-create',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './role-create.component.html',
+  styleUrls: ['./role-create.component.css']
+})
+export class RoleCreateComponent implements OnInit{
+
+  departments: Department[] = [];
+
+  roleForm: FormGroup = new FormGroup({
+    roleName: new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+    departmentID: new FormControl("", [Validators.required])
+  });
+
+  constructor(
+    private _roleService: RoleService,
+    private _departmentService: DepartmentService
+  ) {}
+
+  ngOnInit(): void {
+    this.getDepartments();
+  }
+
+  /** Get departments from Department Service */
+  getDepartments(): void {
+    this.departments = this._departmentService.getDepartments();
+  }
+
+  onSubmit(): void {
+    if (this.roleForm.valid) {
+      console.log('Form Submitted:', this.roleForm.value);
+      this._roleService.addRole(this.roleForm.value);
+      this.roleForm.reset();
+    }
+  }
+}
